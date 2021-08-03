efb416ecd0e468e275b68010f74415e2b587487a
Prashant Dwivedi <prashantdwivedi194@gmail.com>
2021-08-03 23:27:44 +0530

    Updated the Connection URL

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ app.get('/',(req, res)=>{
 })
 
 const PORT = process.env.PORT || 5000; //here we use 5000 OR the other one, which will be used while deployment
-const CONNECTION_URL = 'mongodb://localhost:27017/esd';
+// const CONNECTION_URL = 'mongodb://localhost:27017/esd';
+const CONNECTION_URL = 'mongodb+srv://Prashant:Prashant123@cluster0.myzaa.mongodb.net/esd?retryWrites=true&w=majority';
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`)))
