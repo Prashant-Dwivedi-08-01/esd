ea84cacfa604be4ed24d81e5cd610fd970a6668c
Prashant Dwivedi <prashantdwivedi194@gmail.com>
2021-08-04 14:44:46 +0530

    Updated few endpoints

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import mongoose from 'mongoose';
 import apiRouter from "./api/index.js";
 import { endpoints } from './controllers/index.js';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json({extended: true }));
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use('/api', apiRouter);
 
