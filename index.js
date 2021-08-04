676412a437e27dff0b1e0d6bc7493443c7e58845
Prashant Dwivedi <prashantdwivedi194@gmail.com>
2021-08-04 22:00:59 +0530

    Final Commit

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ import mongoose from 'mongoose';
 import apiRouter from "./api/index.js";
 import { endpoints } from './controllers/index.js';
 import cors from 'cors';
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
 app.use(express.json({extended: true }));
 app.use(express.urlencoded({ extended: true }));
@@ -16,9 +18,9 @@ app.get('/',endpoints)
 
 const PORT = process.env.PORT || 5000; //here we use 5000 OR the other one, which will be used while deployment
 // const CONNECTION_URL = 'mongodb://localhost:27017/esd';
-const CONNECTION_URL = 'mongodb+srv://Prashant:Prashant123@cluster0.myzaa.mongodb.net/esd?retryWrites=true&w=majority';
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
